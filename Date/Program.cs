fa922b45d9dcaa36f931f3299a77f3783068e5f7
troubleman96 <itslugenge96@gmail.com>
2024-12-02 23:02:55 +0300

    trial n error

---
+++ Date/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DatenTime
+{
+    class MyClass
+        
+        
+        
+    {
+        static void Main(string[] arg)
+        {
+            DateTime today = new DateTime(2024, 11, 27);
+            Console.WriteLine("today: {0}", today);
+
+            TimeSpan lunchTime = new TimeSpan(12, 20, 0);
+            Console.WriteLine($"time: {lunchTime}");
+        }
+    }
+}
